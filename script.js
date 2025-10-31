53c01ac2874492ff5351997e23e6cc1f2a14bb07
Shih-Yi Yang <syeven143818@gmail.com>
2025-10-31 21:41:20 +0800

    message API 內容更新

--- script.js
+++ script.js
@@ -490,7 +490,7 @@ submitThoughtBtn.addEventListener('click', async () => {
     // 儲存資料
     await saveData();
 
-    sendLineNotification(`🔥 離職念頭 +1！目前進度：${currentCount} / ${targetCount}。\n內容：${thoughtContent}`);
+    sendLineNotification(`🔥 離職念頭+1! \n目前進度: ${currentCount}/${targetCount}。\n內容：${thoughtContent}`);
     
     // 檢查是否達成目標
     if (currentCount >= targetCount) {
